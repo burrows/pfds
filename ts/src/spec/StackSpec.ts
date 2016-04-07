9c6f04e4be8c929c396b965371f0ee9395562c07
Corey Burrows <corey.burrows@centro.net>
2016-04-07 13:09:15 -0500

    Setup typescript.

---
+++ ts/src/spec/StackSpec.ts
@@ -0,0 +1,13 @@
+/// <reference path="../types/jasmine.d.ts" />
+
+import Stack from "../Stack";
+
+export default describe('Stack', () => {
+  it('foo', () => {
+    expect(Stack.foo).toBe(9);
+  });
+
+  it('bar', () => {
+    expect(1).toBe(1);
+  });
+});
