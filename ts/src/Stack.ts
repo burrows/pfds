0d7658bf300eeaeaff9c85d86b5349dd905e2b2e
Corey Burrows <corey.burrows@centro.net>
2016-04-09 10:05:00 -0500

    Typescript Stack implemention.

--- ts/src/Stack.ts
+++ ts/src/Stack.ts
@@ -1,1 +1,42 @@
-export default {foo: 9};
+export class Empty {
+}
+
+export class Cons<T> {
+  constructor(public head: T, public tail: Stack<T>) {
+  }
+}
+
+export type Stack<T> = Empty | Cons<T>;
+
+export function empty<T>(): Stack<T> {
+  return new Empty;
+}
+
+export function singleton<T>(a: T): Stack<T> {
+  return new Cons(a, empty());
+}
+
+export function isEmpty<T>(stack: Stack<T>): boolean {
+  return stack instanceof Empty;
+}
+
+export function cons<T>(a: T, stack: Stack<T>): Stack<T> {
+  return new Cons(a, stack);
+}
+
+export function head<T>(stack: Stack<T>): T {
+  return stack instanceof Cons ? stack.head : null;
+}
+
+export function tail<T>(stack: Stack<T>): Stack<T> {
+  return stack instanceof Cons ? stack.tail : null;
+}
+
+export function toArray<T>(stack: Stack<T>): T[] {
+  if (isEmpty(stack)) {
+    return [];
+  }
+  else {
+    return [head(stack)].concat(toArray(tail(stack)));
+  }
+}
--- ts/src/spec/StackSpec.ts
+++ ts/src/spec/StackSpec.ts
@@ -1,13 +1,69 @@
 /// <reference path="../types/jasmine.d.ts" />
 
-import Stack from "../Stack";
+import {Stack, Empty, Cons, empty, singleton, isEmpty, cons, head, tail, toArray} from "../Stack";
 
 export default describe('Stack', () => {
-  it('foo', () => {
-    expect(Stack.foo).toBe(9);
+  describe('empty', () => {
+    it('returns an instance of the Empty constructor', () => {
+       expect(empty() instanceof Empty).toBe(true);
+    });
   });
 
-  it('bar', () => {
-    expect(1).toBe(1);
+  describe('isEmpty', () => {
+    it('returns true for an empty stack', () => {
+      expect(isEmpty(empty())).toBe(true);
+    });
+
+    it('returns false for a non-empty stack', () => {
+      expect(isEmpty(singleton(3))).toBe(false);
+    });
+  });
+
+  describe('singleton', () => {
+    it('returns a 1 item stack with the given item', () => {
+      expect(singleton(9)).toEqual(cons(9, empty()));
+    });
+  });
+
+  describe('head', () => {
+    it('returns null on an empty stack', () => {
+      expect(head(empty())).toBeNull();
+    });
+
+    it('returns the item on the top of a singleton stack', () => {
+      expect(head(singleton(9))).toBe(9);
+    });
+
+    it('returns the item on the top of a multi item stack', () => {
+      expect(head(cons(1, cons(2, cons(3, empty()))))).toBe(1);
+    });
+  });
+
+  describe('tail', () => {
+    it('returns null on an empty stack', () => {
+      expect(tail(empty())).toBeNull();
+    });
+
+    it('returns an instance of Empty on a singleton stack', () => {
+      expect(tail(singleton(9)) instanceof Empty).toBe(true);
+    });
+
+    it('returns all but the first item of a multi item stack', () => {
+      expect(tail(cons(1, cons(2, cons(3, empty()))))).toEqual(cons(2, cons(3, empty())));
+    });
+  });
+
+  describe('toArray', () => {
+    it('returns an empty array when given an empty stack', () => {
+      expect(toArray(empty())).toEqual([]);
+    });
+
+    it('returns an array containing the item in a singleton stack', () => {
+      expect(toArray(singleton(4))).toEqual([4]);
+    });
+
+    it('returns an array containing all items of a multi-item stack', () => {
+      expect(toArray(cons(1, cons(2, cons(3, empty()))))).toEqual([1, 2, 3]);
+    });
   });
 });
